22bc096f6b7330fc185bacf42031643cab733036
João <jl95terceira@gmail.com>
2026-01-10 16:35:03 +0000

    demo

---
+++ src/main/java/jl95/net/io/demo/Receive.java
@@ -0,0 +1,26 @@
+package jl95.net.io.demo;
+
+import jl95.lang.P;
+import jl95.net.io.Receiver;
+
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.nio.charset.StandardCharsets;
+
+public class Receive {
+    public static void main(String[] args) throws Exception {
+        var server = new ServerSocket();
+        server.bind(new InetSocketAddress("127.0.0.1", 4242));
+        System.out.println("Bound");
+        var sock = server.accept();
+        System.out.println("Accepted");
+        server.close();
+        var recv = Receiver.of(sock.getInputStream());
+        var toStop = new P<>(false);
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> toStop.set(true)));
+        recv.recvWhile(data -> {
+            System.out.println("<<< " + new String(data, StandardCharsets.UTF_8));
+            return !toStop.get();
+        });
+    }
+}
